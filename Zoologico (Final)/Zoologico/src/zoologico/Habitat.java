967dc6443c092dd7abf2ab42d35f44677b001c4e
DougAgüero <127145828+DougAguero@users.noreply.github.com>
2024-04-17 20:06:45 -0600

    Proyecto Final

---
+++ Zoologico (Final)/Zoologico/src/zoologico/Habitat.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package zoologico;
+
+public class Habitat {
+    private String nombre;
+    private String tipoAmbiente;
+    private int capacidad;
+    
+    public Habitat(String nombre, String tipoAmbiente, int capacidad) {
+        this.nombre = nombre;
+        this.tipoAmbiente = tipoAmbiente;
+        this.capacidad = capacidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipoAmbiente() {
+        return tipoAmbiente;
+    }
+
+    public void setTipoAmbiente(String tipoAmbiente) {
+        this.tipoAmbiente = tipoAmbiente;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+}
